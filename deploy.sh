078bd2ce9dd2b44c4e496dea5cd4b11e8fea919b
skuze <skuzevanov@gmail.com>
2026-01-12 16:06:46 +0500

    Скрипт для деплоя.

--- deploy.sh
+++ deploy.sh
@@ -6,35 +6,37 @@ RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
 BLUE="\e[34m"
+MAGENTA="\e[35m"
 CYAN="\e[36m"
 RESET="\e[0m"
+
+block() {
+    echo -e "${MAGENTA}\n===== $1 =====${RESET}"
+}
 
 echo -e "${CYAN}=== DEPLOY START ===${RESET}"
 echo -e "${BLUE}Directory:${RESET} $(pwd)"
 echo -e "${BLUE}Time:${RESET} $(date)"
-echo "----------------------"
 
-echo -e "${YELLOW}→ Pulling latest changes...${RESET}"
+block "GIT PULL"
 git pull origin main
 
-echo -e "${YELLOW}→ Installing composer dependencies...${RESET}"
-#php84 composer.phar install --no-interaction --prefer-dist --optimize-autoloader
+block "COMPOSER INSTALL"
 /opt/php/8.4/bin/php composer.phar install --no-interaction --prefer-dist --optimize-autoloader
 
-echo -e "${YELLOW}→ Clearing Laravel caches...${RESET}"
+block "CLEARING CACHES"
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-echo -e "${YELLOW}→ Rebuilding Laravel caches...${RESET}"
+block "REBUILDING CACHES"
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-echo -e "${YELLOW}→ Running optimize...${RESET}"
+block "OPTIMIZE"
 php artisan optimize
 
-echo "----------------------"
-echo -e "${GREEN}=== DEPLOY COMPLETE ===${RESET}"
+echo -e "${GREEN}\n=== DEPLOY COMPLETE ===${RESET}"
 echo -e "${BLUE}Time:${RESET} $(date)"
