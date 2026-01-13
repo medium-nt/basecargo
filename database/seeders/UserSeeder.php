05ff649c770e1af281a7e0779d764fcd8fa57023
skuze <skuzevanov@gmail.com>
2026-01-13 18:17:30 +0500

    Добавил таблицу в БД и миграцию для Ролей. Переименовал все миграции (чтобы в правильном порядке запускались).

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Админ',
+            'email' => '1@1.ru',
+            'password' => bcrypt('111111'),
+            'role_id' => 3,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Агент 1',
+            'email' => '2@2.ru',
+            'password' => bcrypt('222222'),
+            'role_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Агент 2',
+            'email' => '22@22.ru',
+            'password' => bcrypt('222222'),
+            'role_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Клиент 1',
+            'email' => '3@3.ru',
+            'password' => bcrypt('333333'),
+            'role_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Клиент 2',
+            'email' => '33@33.ru',
+            'password' => bcrypt('333333'),
+            'role_id' => 1,
+        ]);
+    }
+}
